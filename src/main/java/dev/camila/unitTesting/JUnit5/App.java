42720bc2c4a69e6267b7914a8ceca8d0e38c3777
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-05-25 07:58:10 -0300

    first commit

---
+++ src/main/java/dev/camila/unitTesting/JUnit5/App.java
@@ -0,0 +1,8 @@
+package dev.camila.unitTesting.JUnit5;
+
+public class App {
+    
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ src/test/java/dev/camila/unitTesting/JUnit5/AppTest.java
@@ -0,0 +1,13 @@
+package dev.camila.unitTesting.JUnit5;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
